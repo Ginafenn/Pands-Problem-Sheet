443b0ad3b987e5163517959676e32e63d5d2ff66
Fennessy <regina.fennessy@bsci.com>
2022-02-11 19:09:21 +0000

    Week03

---
+++ Week03/secondstring.py
@@ -0,0 +1,5 @@
+#This program is to input a string and outputs every second letter in reverse order
+#author:Regina Fennessy
+
+sentence = input("Input a sentence that you want reversed:")
+print(sentence[::-2])
