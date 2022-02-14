3006924d782a7957ce462f8ac0f0651a82bf7efe
Fennessy <regina.fennessy@bsci.com>
2022-02-14 17:32:13 +0000

    Week04

---
+++ Week04/collatz.py
@@ -0,0 +1,15 @@
+#This program asks the user to input any positive integer and outputs the successive values of the following calculation
+#author:Regina Fennessy
+
+Value = int(input())
+
+if (Value % 2) == 0:
+    print(Value, (Value / 2))
+
+if (Value % 2) == 0:
+    print(Value, (Value / 2))
+
+else:
+    print(Value, (Value / 3)+1)
+
+    
