de077f482b7bdbee2829b6ba69e70c662fa1a8b8
Ginafenn <regina.fennessy@bsci.com>
2022-02-15 07:48:17 +0000

    'Week04'

--- Week04/collatz.py
+++ Week04/collatz.py
@@ -1,15 +1,19 @@
 #This program asks the user to input any positive integer and outputs the successive values of the following calculation
+#https://www.webucator.com/article/collatz-conjecture-in-python/
 #author:Regina Fennessy
 
-Value = int(input())
+def collatz(number):
+    while number != 1: #
+        print(number)
+        if number % 2 == 0:
+            number = int(number / 2)
+        else:
+            number = int(3 * number + 1)
+    else:
+        print(number)
 
-if (Value % 2) == 0:
-    print(Value, (Value / 2))
 
-if (Value % 2) == 0:
-    print(Value, (Value / 2))
-
-else:
-    print(Value, (Value / 3)+1)
-
-    
+def main():
+    number = int(input('Please enter a positive integer: '))
+    collatz(number)
+main()
