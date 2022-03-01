e7b5dfaed9a1c14b2008af3a1ab717fc09e2bcd4
Ginafenn <regina.fennessy@bsci.com>
2022-03-01 21:25:46 +0000

    Week 06 worksheet

---
+++ Week06/squareroot.py
@@ -0,0 +1,10 @@
+
+
+# Python Program that takes a positive floating-point number as input and outputs an approximation of its square root
+
+import math
+n = float(input("Please enter a positive number:"))
+
+result = math.sqrt(n)
+
+print("The square root of " , n,  "is approx."  ,result)
